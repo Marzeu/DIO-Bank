ac196ca3105884e615102f485b387319cca75b89
Marzeu <marzahell@hotmail.com>
2021-02-02 17:01:49 -0300

    menu de usuario

--- Program.cs
+++ Program.cs
@@ -1,14 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DIO.Bank
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Marzeu Selau");                       
-            
-            Console.WriteLine(minhaConta.ToString());
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        //ListarContas();
+                        break;
+                    case "2":
+                       // InserirConta();
+                        break;
+                    case "3":
+                      //  Transferir();
+                        break;
+                    case "4":
+                      //  Sacar();
+                        break;
+                    case "5":
+                     //   Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
         }
 
         private static string ObterOpcaoUsuario()
